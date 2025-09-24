c8a60aede3c61ec67d38abe1abadb17530068df0
Lidar Holding Group S. A <info@nt.gt>
2025-09-24 04:17:44 +0000

    programas sociales

---
+++ sam/sam/doctype/programa_social_y_evento_detalle/programa_social_y_evento_detalle.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Lidar Holding Group S. A. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ProgramaSocialyEventoDetalle(Document):
+	pass
