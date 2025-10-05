0d42e1832cb796d5e64e9f844046f0a2e5b2f518
Lidar Holding Group S. A <info@nt.gt>
2025-10-05 16:31:50 +0000

    Actualizaciones SOSEA Release V1

---
+++ sam/sam/doctype/estudio_socio_economico_enfermedad/estudio_socio_economico_enfermedad.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Lidar Holding Group S. A. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Estudio Socio Economico Enfermedad", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ sam/sam/doctype/padron_base/padron_base.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Lidar Holding Group S. A. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Padron Base", {
+// 	refresh(frm) {
+
+// 	},
+// });
