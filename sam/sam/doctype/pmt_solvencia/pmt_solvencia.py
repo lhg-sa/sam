efc551ca8ec692a6d748fedcb667ef9a413c6320
“lhg-sa” <info@nt.gt>
2025-11-13 04:56:46 +0000

    Publicacion Beta V1 PMT

---
+++ sam/sam/doctype/pmt_solvencia/pmt_solvencia.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, Lidar Holding Group S. A. and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class PMTSolvencia(Document):
+	pass
