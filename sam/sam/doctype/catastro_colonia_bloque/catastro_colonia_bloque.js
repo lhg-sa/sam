a442f8134a424388448478c9b483b07364df7e1e
“lhg-sa” <info@nt.gt>
2025-11-19 03:44:14 +0000

    Pre Catastro Base Alpha V1

---
+++ sam/sam/doctype/catastro_colonia_bloque/catastro_colonia_bloque.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Lidar Holding Group S. A. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("catastro_colonia_bloque", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ sam/sam/doctype/dmp_zona/test_dmp_zona.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Lidar Holding Group S. A. and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDMPZona(FrappeTestCase):
+	pass
