43852f5f44a63fa7524ccb0881484c4b2aaec921
LHG S. A <info@lhg.com.gt>
2026-03-01 23:11:38 +0000

    Ajuste para ordenamiento DMP

--- sam/sam/doctype/dmp_inmueble_movimiento/dmp_inmueble_movimiento.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Lidar Holding Group S. A. and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class dmp_inmueble_movimiento(Document):
-	pass
